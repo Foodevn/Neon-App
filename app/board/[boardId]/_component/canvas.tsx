06a337592463aa1ac4b85df89c83731db2a67dd5
Foodevn <hoangphuc0918065630@gmail.com>
2025-07-03 21:21:02 +0700

    feat: Room Authentication
    Signed-off-by: Foodevn <hoangphuc0918065630@gmail.com>

--- app/board/[boardId]/_component/canvas.tsx
+++ app/board/[boardId]/_component/canvas.tsx
@@ -3,12 +3,16 @@ import {Info} from "@/app/board/[boardId]/_component/info";
 import {Toolbar} from "@/app/board/[boardId]/_component/toolbar";
 import {Participants} from "@/app/board/[boardId]/_component/participants";
 
+import {useSelf} from "@/liveblocks.config";
+
+
 interface CanvasProps {
     boardId: string;
 }
 
 export const Canvas = ({boardId}:CanvasProps) => {
-
+    const info=useSelf(me=>me.info);
+    console.log(info);
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
 
