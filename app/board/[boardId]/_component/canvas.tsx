03aa472dd2d9e9eee81f9d283143bc21b7002b13
Foodevn <hoangphuc0918065630@gmail.com>
2025-07-04 18:42:42 +0700

    feat: Cursors Presence
    Signed-off-by: Foodevn <hoangphuc0918065630@gmail.com>

--- app/board/[boardId]/_component/canvas.tsx
+++ app/board/[boardId]/_component/canvas.tsx
@@ -3,9 +3,11 @@ import {Info} from "@/app/board/[boardId]/_component/info";
 import {Toolbar} from "@/app/board/[boardId]/_component/toolbar";
 import {Participants} from "@/app/board/[boardId]/_component/participants";
 
-import {useHistory,useCanUndo,useCanRedo } from "@/liveblocks.config";
-import {CanvasMode, CanvasState} from "@/types/canvas";
-import {useState} from "react";
+import {useCanRedo, useCanUndo, useHistory, useMutation} from "@/liveblocks.config";
+import {Camera, CanvasMode, CanvasState} from "@/types/canvas";
+import {useCallback, useState} from "react";
+import {CursorsPresence} from "@/app/board/[boardId]/_component/cursors-presence";
+import {pointerEventToCanvasPoint} from "@/lib/utils";
 
 interface CanvasProps {
     boardId: string;
@@ -17,11 +19,33 @@ export const Canvas = ({boardId}:CanvasProps) => {
     const [canvasState, setCanvasState] = useState<CanvasState>({
         mode: CanvasMode.None
     });
+    const [camera, setCamera] = useState<Camera>({x: 0, y: 0});
 
     const history=useHistory();
     const canUndo=useCanUndo();
     const canRedo=useCanRedo();
 
+    const onWheel = useCallback((e: React.WheelEvent) => {
+        setCamera((camera) => ({
+            x: camera.x - e.deltaX,
+            y: camera.y - e.deltaY,
+        }));
+    }, []);
+
+    const onPointerMove = useMutation(
+        ({setMyPresence}, e: React.PointerEvent) => {
+            e.preventDefault();
+
+            const current = pointerEventToCanvasPoint(e, camera);
+            setMyPresence({cursor: current});
+        }, []
+    );
+    const onPointerLeave = useMutation(
+        ({setMyPresence}) => {
+            setMyPresence({cursor: null});
+        },
+        [],
+    );
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
 
@@ -34,6 +58,17 @@ export const Canvas = ({boardId}:CanvasProps) => {
                 canUndo={canRedo}
                 undo={history.undo}
                 redo={history.redo}/>
+
+            <svg
+                className={"h-[100vh] w-[100vw]"}
+                onWheel={onWheel}
+                onPointerMove={onPointerMove}
+                onPointerLeave={onPointerLeave}
+            >
+                <g>
+                    <CursorsPresence/>
+                </g>
+            </svg>
         </main>
     )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,15 @@
-import {clsx, type ClassValue} from "clsx"
+import {type ClassValue, clsx} from "clsx"
 import {twMerge} from "tailwind-merge"
+import {Camera} from "@/types/canvas";
 
 
-const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
+const COLORS = [
+    "#DC2626",
+    "#D97706",
+    "#059669",
+    "#7C3AED",
+    "#DB2777"
+];
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
@@ -10,4 +17,14 @@ export function cn(...inputs: ClassValue[]) {
 
 export function connectionIdToColor(connectionId: number): string {
     return COLORS[connectionId % COLORS.length];
+}
+
+export function pointerEventToCanvasPoint(
+    e: React.PointerEvent,
+    camera: Camera,
+) {
+    return {
+        x: Math.round(e.clientX) - camera.x,
+        y: Math.round(e.clientY) - camera.y,
+    };
 }
